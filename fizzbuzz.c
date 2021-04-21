abf9e312064e856fd9eeebdebb77d41ed04002c6
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2021-04-21 10:13:27 -0400

    Create fizzbuzz.c

---
+++ fizzbuzz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+FizzBuzz in C
+Written by calebrwalk5 (aka anusO1)
+*/
+
+int main() {
+    int i;
+    for (i = 1; i <100; i++) {
+        if (i % 3 == 0) {
+            printf ("Fizz\n");
+        } else if ( i % 5 == 0) {
+            printf ("Buzz\n");
+        } else if (i % 15 == 0) {
+            printf ("FizzBuzz\n");
+        } else {
+            printf("%d\n",i);
+        }
+    } return 0;
+}
